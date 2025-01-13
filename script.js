d73509d8a99bf9af706c5c110c69d4d22123a389
J-max <joelmtn7@gmail.com>
2025-01-13 21:46:08 +0000

    more style nd initial function

--- script.js
+++ script.js
@@ -8,6 +8,33 @@ let fighting;
 let monsterHealth;
 let inventory = ['sticks'];
 
+// controls
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector('#button2');
 const button3 = document.querySelector('#button3');
+
+// text box
+const text = document.querySelector('#text');
+
+//xp top bar
+const xpText = document.querySelector('#xpText');
+
+//your health
+const healthText = document.querySelector('#healthText');
+
+//your gold number
+const goldText = document.querySelector('#goldText');
+
+// monster div
+const monsterStats = document.querySelector('#monsterStats');
+
+
+//#monster stat #monster name
+const monsterName = document.querySelector('#monsterName');
+
+const monsterHealthText = document.querySelector('#monsterHealth');
+
+
+function goStore() {
+    
+}
