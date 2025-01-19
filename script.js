65ef63529027b3b325112216f922dfc4cff77697
J-max <joelmtn7@gmail.com>
2025-01-19 21:47:09 +0000

    19/1/25

--- script.js
+++ script.js
@@ -39,28 +39,28 @@ const monsterHealthText = document.querySelector('#monsterHealth');
 // initialize buttons
 
 function goTown() {
-    button1.innerText = 'Go to store';
-    button2.innerText = 'Go to cave';
-    button3.innerText = 'Fight dragon';
+    // button1.innerText = 'Go to store';
+    // button2.innerText = 'Go to cave';
+    // button3.innerText = 'Fight dragon';
 
-    button1.onclick = goStore;
-    button2.onclick = goCave;
-    button3.onclick = fightDragon;
+    // button1.onclick = goStore;
+    // button2.onclick = goCave;
+    // button3.onclick = fightDragon;
 
-    text.innerText = 'You are in the town square. You see a sign that says \"Store\".';
+    // text.innerText = 'You are in the town square. You see a sign that says \"Store\".';
 }
 
 function goStore() {
     // console.log('Going to store.');
-    button1.innerText = 'Buy 10 health (10 gold)';
-    button2.innerText = 'Buy weapon (30 gold)';
-    button3.innerText = 'Go to town square';
+    // button1.innerText = 'Buy 10 health (10 gold)';
+    // button2.innerText = 'Buy weapon (30 gold)';
+    // button3.innerText = 'Go to town square';
 
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
+    // button1.onclick = buyHealth;
+    // button2.onclick = buyWeapon;
+    // button3.onclick = goTown;
 
-    text.innerText = 'You enter the store.';
+    // text.innerText = 'You enter the store.';
 }
 function goCave() {
     console.log("Going to cave.");
@@ -89,11 +89,39 @@ function buyWeapon() {
 }
 
 function update (location) {
+    button1.innerText = 'Go to store';
+    button2.innerText = 'Go to cave';
+    button3.innerText = 'Fight dragon';
 
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+
+    text.innerText = 'You are in the town square. You see a sign that says \"Store\".';
 }
 
 const locations = [
-    {name: "town square", "button text":['Go to store', 'Go to cave', 'Fight dragon']}
+    {
+        name: "town square", 
 
+        "button text":['Go to store', 'Go to cave', 'Fight dragon'], 
+
+        "button functions":[goStore, goCave, fightDragon],
+
+        text: 'You are in the town square. You see a sign that says \"Store\".'
+    },
+
+    {
+        name: "store",
+
+        "button text": ['Buy 10 health (10 gold)', 'Buy weapon (30 gold)','Go to town square'],
+
+        "button functions":[buyHealth, buyWeapon, goTown],
+
+        text: 'You enter the store.'
+
+    }
+
+    
 ];
 
