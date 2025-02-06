0d43c29610869aabfdcb5df6cae28081f57830a6
J-max <joelmtn7@gmail.com>
2025-02-06 23:05:40 +0100

    javascript getting bporing

--- script.js
+++ script.js
@@ -265,7 +265,11 @@ function fightDragon() {
 }
 
 function attack() {
+text.innerText = "The " + monsters[fighting].name + " attacks. " ;
 
+text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + "." ;
+
+health -= monsters[fighting].level
 };
 
 function dodge() {
